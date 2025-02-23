ec337162cced4b305b817f4f3b7597fcf7609c4c
Cauê Ferreira Lacerda <cauef.lacerda@gmail.com>
2025-02-23 18:24:32 -0300

    feat: implementa subtracao no menu

--- intro_git/menu.py
+++ intro_git/menu.py
@@ -1,9 +1,11 @@
 from calculadora import somar,subtrair,multiplicar,dividir
 def menu(opcao):
     while opcao != 0:
-        opcao = int(input("1-Somar\n0-sair\n"))
+        opcao = int(input("1-Somar\n2-Subtrair\n0-Sair\n "))
         if opcao == 1:
             print(f"A soma entre {n1} e {n2} é {somar(n1,n2)}")
+        elif opcao == 2:
+            print(f"A subtração entre {n1} e {n2} é {subtrair(n1,n2)}")
     print("Obrigado por usar a calculadora")
 opcao = 5
 n1 = int(input("Entre com um número: "))
